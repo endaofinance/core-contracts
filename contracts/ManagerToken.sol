16e65a8c84b595d76a4296cf6ec5c929b43b28d2
James Smoot <jimi@jsfour.com>
2022-03-18 11:20:10 -0700

    basic deposit logic

--- contracts/ManagerToken.sol
+++ contracts/ManagerToken.sol
@@ -5,85 +5,136 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+//import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 
-contract ManagerToken is AccessControlEnumerable, ERC20Capped {
-  bytes32 public constant CLAIMER_ROLE = keccak256("CLAIMER_ROLE");
-  uint256 _cap;
-  uint256 _targetReserve;
-  uint256 _mgmtFee;
-  uint256 _feeBasis;
+// Dev deps
+import "hardhat/console.sol";
 
-  constructor(string memory name_, string memory symbol_, uint256 cap_, uint256 mgmtFee_) ERC20(name_, symbol_) ERC20Capped(cap_) { 
-    _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-    _setupRole(CLAIMER_ROLE, _msgSender());
-    _mgmtFee = mgmtFee_; // Fee in thousands
-    _feeBasis = 1000;
-  }
+contract ManagerToken is AccessControlEnumerable, ERC20 {
+    IQuoter public constant _quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
+    bytes32 public constant CLAIMER_ROLE = keccak256("CLAIMER_ROLE");
+    uint256 _targetReserve;
+    uint256 _mgmtFee;
+    uint256 _feeBasis;
+    address immutable _baseToken;
+    address immutable _quoteToken;
+    event HardBurn(uint256 amount);
 
-  function mint(address account, uint256 amount) public virtual {
-    super._mint(account, amount);
-  }
+    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(CLAIMER_ROLE, _msgSender());
+        //_mgmtFee = mgmtFee_; // Fee in thousands
+        _baseToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // weth
+        _quoteToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // usdc
+        //super._mint(_msgSender(), 50);
+        // 18 decimals by default
+    }
 
-  // Return the value of the contract in eth
-  function totalEquity() public virtual returns uint256 {
-    // TODO:
-    return 1000
-  }
+    receive() external payable {
+        // determine how mucch equity there is
+        // determine how much user is depositing
+        // accept deposit for the max amount
+        // return leftover eth
+        uint256 price_;
+        uint256 supply_ = totalSupply();
 
-  function _calculateEquity(uint256 tokenAmount) private virtual returns uint256 {
-    uint256 nonFeeAmount = tokenAmount * _mgmtFee / _feeBasis;
-    uint256 totalValue = getValue();
-    return nonFeeAmount/totalSupply() * totalValue;
-  }
+        if (supply_ == 0) {
+            // Initial price
+            price_ = 1000000000000000000; // 1 eth
+        } else {
+            uint256 value_ = totalValue() - msg.value; // Pre money value
+            console.log("supply", supply_);
+            console.log("value", value_);
+            price_ = value_ / supply_;
+        }
 
-  function myEquity() public virtual returns uint256 {
-    uint256 amount = balanceOf(_msgSender());
-    return _calculateEquity(amount)
-  }
+        console.log("price", price_);
+        console.log("depositAmount", msg.value);
+        uint256 amount = msg.value / price_;
+        console.log("amount", amount);
 
-  function burn(uint256 amount) public virtual {
-    uint256 callerEquity = myEquity();
+        super._mint(_msgSender(), amount);
+    }
 
-    uint256 reserveBalance = address(this).balance;
-    require(reserveBalance >= callerEquity, "Reserve does not have enough balance, try calling hardBurn");
+    function price() public view virtual returns (uint256) {
+        uint256 supply_ = totalSupply();
+        console.log("total supply", supply_);
 
-    _msgSender().transfer(callerEquity);
-    super._burn(_msgSender(), amount); // TODO: can msg.sender be a 0 address?
-  }
+        if (supply_ == 0) {
+            // Initial price
+            return 1000000000000000000; // 1 eth
+        }
 
-  // Execute rebalance witholding equity amount and then burns the token
-  function hardBurn(uint256 tokenAmount) public virtual {
-    uint256 targetEquity = _calculateEquity(tokenAmount);
-    require(targetEquity >= totalEquity(), "Not enough equity for a hardBurn");
-    rebalance(targetEquity);
-    burn(tokenAmount);
-    emit HardBurn(amount);
-  }
+        uint256 value_ = totalValue();
+        return value_ / supply_;
+    }
 
-  function rebalance(uint256 additionalEquity) public virtual {
-    // Calculate midprice of pair
-    // + / - a target % gain
-    // Sell everything
-    // withold reserve
+    function burn(uint256 amount) public virtual {
+        uint256 callerEquity = myEquity();
 
-  }
+        uint256 reserveBalance = address(this).balance;
+        require(
+            reserveBalance >= callerEquity,
+            "Reserve does not have enough balance, try calling hardBurn"
+        );
 
-  function claimFee(uint256 percentage) public virtual {
-    require(hasRole(CLAIMER_ROLE), _msgSender(), "Public key can not claimFee");
-    uint256 myBalance = balanceOf(address(this));
-    transfer(address(this), myBalance*percentage/100);
-  }
+        transfer(_msgSender(), callerEquity);
+        super._burn(_msgSender(), amount); // TODO: can msg.sender be a 0 address?
+    }
 
-  //function ccap() public returns (uint256) {
-    //return _cap;
-  //}
+    // Execute rebalance witholding equity amount and then burns the token
+    function hardBurn(uint256 tokenAmount) public virtual {
+        uint256 targetEquity = _calculateEquity(tokenAmount);
+        require(targetEquity >= totalValue(), "Not enough equity for a hardBurn");
+        rebalance(targetEquity);
+        burn(tokenAmount);
+        emit HardBurn(tokenAmount);
+    }
 
-  
-  //function rebalance() public virtual {
-    //// check to see if sender can call rebalance
-    //// sell everything & set aside reserve
-    //// re enter everything
+    // Return the value of the contract in wei
+    function totalValue() public view virtual returns (uint256) {
+        uint256 lpValue = 0;
 
-  //}
+        return lpValue + address(this).balance;
+        // TODO:
+        //uint256 quoteBalance = 0; // TODO: get balance of quote
+        //uint256 baseBalance = 100; // TODO: gjt balance of base
+        //uint24 fee = 3000;
+        //uint160 sqrtPriceLimitX96 = 0;
+        //uint256 quoteValue = _quoter.quoteExactInputSingle(
+        //_baseToken,
+        //_quoteToken,
+        //fee,
+        //baseBalance,
+        //sqrtPriceLimitX96
+        //);
+        //return baseBalance + quoteValue;
+        //// Add oge all eth
+    }
 
+    function _calculateEquity(uint256 tokenAmount) private returns (uint256) {
+        uint256 nonFeeAmount = (tokenAmount * _mgmtFee) / _feeBasis;
+        uint256 _totalValue = totalValue();
+        return (nonFeeAmount / totalSupply()) * _totalValue;
+    }
+
+    function myEquity() public virtual returns (uint256) {
+        uint256 amount = balanceOf(_msgSender());
+        return _calculateEquity(amount);
+    }
+
+    function rebalance(uint256 additionalEquity) public virtual {
+        // Calculate midprice of pair
+        // + / - a target % gain
+        // Sell everything
+        // withold reserve
+        // safe approve
+    }
+
+    function claimFee(uint256 percentage) public virtual {
+        require(hasRole(CLAIMER_ROLE, _msgSender()), "Public key can not claimFee");
+        uint256 myBalance = balanceOf(address(this));
+        transfer(address(this), (myBalance * percentage) / 100);
+    }
 }
--- test/manager.js
+++ test/manager.js
@@ -1,12 +1,77 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("ManagerToken", function () {
-  it("Should set a cap", async () => {
-    const ManagerToken = await ethers.getContractFactory("ManagerToken");
-    const managerToken = ManagerToken.deploy(100, 10);
-    await managerToken.deployed();
+const ONE_E_18 = ethers.BigNumber.from("1000000000000000000");
 
-    expect(await managerToken.cap()).to.eq(100);
+const toContractNumber = (inNum) => {
+  return ethers.BigNumber.from(inNum).mul(ONE_E_18);
+};
+
+describe("Contract", async () => {
+  let contract;
+  let owner;
+  beforeEach(async () => {
+    const signers = await ethers.getSigners();
+    owner = signers[0];
+    const ManagerToken = await ethers.getContractFactory("ManagerToken");
+    contract = await ManagerToken.deploy("Manager token", "mgr");
+  });
+
+  describe("deploy", async () => {
+    it("deploys correctly", async () => {
+      expect(await contract.totalSupply()).to.equal(0);
+      let currentPrice = await contract.price();
+      expect(currentPrice.toString()).to.equal(toContractNumber("1").toString());
+      let ownerBalance = await contract.balanceOf(owner.address);
+      expect(ownerBalance).to.equal(0);
+    });
+  });
+
+  describe("deposits", async () => {
+    it("deposits correctly", async () => {
+      const [owner, act1, act2, act3, act4] = await ethers.getSigners();
+      const testCases = [
+        {
+          account: act1,
+          deposit: "1.0",
+          expectedPrice: "1",
+          expectedBalance: "1",
+        },
+        {
+          account: act2,
+          deposit: "2.0",
+          expectedPrice: "1",
+          expectedBalance: "2",
+        },
+        {
+          account: act3,
+          deposit: "3.0",
+          expectedPrice: "1",
+          expectedBalance: "3",
+        },
+        {
+          account: act4,
+          deposit: "0.5",
+          expectedPrice: "1",
+          expectedBalance: "0.5",
+        },
+      ];
+      for (let i = 0; i < testCases.length; i++) {
+        const testCase = testCases[i];
+        console.log(testCase.deposit);
+        const acct = testCase.account;
+        let currentPrice = await contract.price();
+        const myExpectedPrice = ethers.BigNumber.from(testCase.expectedPrice).mul(ONE_E_18);
+        expect(currentPrice.toString()).to.equal(myExpectedPrice.toString());
+
+        await acct.sendTransaction({
+          to: contract.address,
+          value: ethers.utils.parseEther(testCase.deposit), // Sends 1.0 ether
+        });
+
+        let userBalance = await contract.balanceOf(acct.address);
+        expect(userBalance).to.equal(testCase.expectedBalance);
+      }
+    });
   });
 });
