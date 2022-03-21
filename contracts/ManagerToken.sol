018c57a5bc0a83b4e8ee4364adbeb528e43ac24a
James Smoot <jimi@jsfour.com>
2022-03-21 11:56:42 -0700

    Sweep rebalancer - Drop cap (we dont need it) - Sweep deposited eth to target address - Inflate supply for benificiary

--- contracts/ManagerToken.sol
+++ contracts/ManagerToken.sol
@@ -5,30 +5,35 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-//import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/IQuoter.sol";
 
 // Dev deps
 import "hardhat/console.sol";
 
 contract ManagerToken is AccessControlEnumerable, ERC20 {
-    IQuoter public constant _quoter = IQuoter(0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6);
-    bytes32 public constant CLAIMER_ROLE = keccak256("CLAIMER_ROLE");
-    uint256 _targetReserve;
-    uint256 _mgmtFee;
-    uint256 _feeBasis;
-    address immutable _baseToken;
-    address immutable _quoteToken;
     event HardBurn(uint256 amount);
+    bytes32 public constant BENEFICIARY_ROLE = keccak256("BENEFICIARY_ROLE");
+    bytes32 public constant REBALANCER_ROLE = keccak256("REBALANCER_ROLE");
+    address _manager;
+    uint256 _targetReserveBips;
+    uint256 _epochDrawBips;
+    uint256 _epochsPerAnum = 12;
 
-    constructor(string memory name_, string memory symbol_) ERC20(name_, symbol_) {
-        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-        _setupRole(CLAIMER_ROLE, _msgSender());
-        //_mgmtFee = mgmtFee_; // Fee in thousands
-        _baseToken = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // weth
-        _quoteToken = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; // usdc
-        //super._mint(_msgSender(), 50);
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 annualDrawBips_,
+        uint256 targetReserveBips_,
+        address manager_
+    ) ERC20(name_, symbol_) {
         // 18 decimals by default
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(BENEFICIARY_ROLE, _msgSender());
+        _setupRole(REBALANCER_ROLE, _msgSender());
+        _epochDrawBips = annualDrawBips_ / _epochsPerAnum;
+        _targetReserveBips = targetReserveBips_;
+        _manager = manager_;
     }
 
     receive() external payable {
@@ -44,23 +49,16 @@ contract ManagerToken is AccessControlEnumerable, ERC20 {
             price_ = 1; // 1 wei
         } else {
             uint256 value_ = totalValue() - msg.value; // Pre money value
-            console.log("supply", supply_);
-            console.log("value", value_);
             price_ = value_ / supply_;
         }
 
-        console.log("price", price_);
-        console.log("depositAmount", msg.value);
         uint256 amount = msg.value / price_;
-        console.log("amount", amount);
 
         super._mint(_msgSender(), amount);
     }
 
     function price() public view virtual returns (uint256) {
         uint256 supply_ = totalSupply();
-        console.log("total supply", supply_);
-
         if (supply_ == 0) {
             // Initial price
             return 1; // 1 wei
@@ -70,27 +68,24 @@ contract ManagerToken is AccessControlEnumerable, ERC20 {
         return value_ / supply_;
     }
 
-    function burn(uint256 amount) public virtual {
-        uint256 callerEquity = myEquity();
+    //function burn(uint256 amount) public virtual {
+    //uint256 callerEquity = myEquity();
 
-        uint256 reserveBalance = address(this).balance;
-        require(
-            reserveBalance >= callerEquity,
-            "Reserve does not have enough balance, try calling hardBurn"
-        );
+    //uint256 reserveBalance = address(this).balance;
+    //require(reserveBalance >= callerEquity, "Reserve does not have enough balance, try calling hardBurn");
 
-        transfer(_msgSender(), callerEquity);
-        super._burn(_msgSender(), amount); // TODO: can msg.sender be a 0 address?
-    }
+    //transfer(_msgSender(), callerEquity);
+    //super._burn(_msgSender(), amount); // TODO: can msg.sender be a 0 address?
+    //}
 
     // Execute rebalance witholding equity amount and then burns the token
-    function hardBurn(uint256 tokenAmount) public virtual {
-        uint256 targetEquity = _calculateEquity(tokenAmount);
-        require(targetEquity >= totalValue(), "Not enough equity for a hardBurn");
-        rebalance(targetEquity);
-        burn(tokenAmount);
-        emit HardBurn(tokenAmount);
-    }
+    //function hardBurn(uint256 tokenAmount) public virtual {
+    //uint256 targetEquity = _calculateEquity(tokenAmount);
+    //require(targetEquity >= totalValue(), "Not enough equity for a hardBurn");
+    //rebalance(targetEquity);
+    //burn(tokenAmount);
+    //emit HardBurn(tokenAmount);
+    //}
 
     // Return the value of the contract in wei
     function totalValue() public view virtual returns (uint256) {
@@ -113,28 +108,31 @@ contract ManagerToken is AccessControlEnumerable, ERC20 {
         //// Add oge all eth
     }
 
-    function _calculateEquity(uint256 tokenAmount) private returns (uint256) {
-        uint256 nonFeeAmount = (tokenAmount * _mgmtFee) / _feeBasis;
-        uint256 _totalValue = totalValue();
-        return (nonFeeAmount / totalSupply()) * _totalValue;
-    }
-
-    function myEquity() public virtual returns (uint256) {
-        uint256 amount = balanceOf(_msgSender());
-        return _calculateEquity(amount);
-    }
-
-    function rebalance(uint256 additionalEquity) public virtual {
+    function rebalance() public virtual {
         // Calculate midprice of pair
         // + / - a target % gain
         // Sell everything
         // withold reserve
         // safe approve
+
+        // Inflate supply so that the benificiary can claim new supply
+        uint256 inflationAmount = (totalSupply() * _epochDrawBips) / 10000;
+        super._mint(address(this), inflationAmount); // Inflate supply to assigm value to the claimers
+
+        uint256 reserveAmount = (address(this).balance * _targetReserveBips) / 10000;
+        uint256 totalAmount = address(this).balance - reserveAmount;
+
+        if (totalAmount <= 1) {
+            return;
+        }
+
+        (bool sent, ) = _manager.call{value: totalAmount}("");
+        require(sent, "Failed to send Ether");
     }
 
-    function claimFee(uint256 percentage) public virtual {
-        require(hasRole(CLAIMER_ROLE, _msgSender()), "Public key can not claimFee");
-        uint256 myBalance = balanceOf(address(this));
-        transfer(address(this), (myBalance * percentage) / 100);
-    }
+    //function claimFee(uint256 percentage) public virtual {
+    //require(hasRole(CLAIMER_ROLE, _msgSender()), "Public key can not claimFee");
+    //uint256 myBalance = balanceOf(address(this));
+    //transfer(address(this), (myBalance * percentage) / 100);
+    //}
 }
--- test/manager.js
+++ test/manager.js
@@ -14,8 +14,15 @@ describe("Contract", async () => {
   beforeEach(async () => {
     const signers = await ethers.getSigners();
     owner = signers[0];
+    manager = signers[10];
     const ManagerToken = await ethers.getContractFactory("ManagerToken");
-    contract = await ManagerToken.deploy("Manager token", "mgr");
+    contract = await ManagerToken.deploy(
+      "Manager token",
+      "mgr",
+      "700",
+      "25",
+      manager.address,
+    );
   });
 
   describe("deploy", async () => {
@@ -59,7 +66,6 @@ describe("Contract", async () => {
       ];
       for (let i = 0; i < testCases.length; i++) {
         const testCase = testCases[i];
-        console.log(testCase.deposit);
         const acct = testCase.account;
         let currentPrice = await contract.price();
         expect(currentPrice.toString()).to.equal(testCase.expectedPrice);
@@ -74,5 +80,54 @@ describe("Contract", async () => {
         expect(userBalance).to.equal(myExpectedBalance.toString());
       }
     });
+  });
+
+  describe("management", async () => {
+    it("rebalances correctly", async () => {
+      const accounts = await ethers.getSigners();
+
+      const owner = accounts[0];
+      const other = accounts[1];
+      const manager = accounts[10];
+
+      await other.sendTransaction({
+        to: contract.address,
+        value: ethers.utils.parseEther("1.0"), // Sends 1.0 ether
+      });
+
+      await contract.rebalance();
+      const mgrBalance =
+        (await manager.getBalance()) + ethers.utils.parseEther("1.0");
+      expect(mgrBalance.toString()).to.equal(mgrBalance);
+
+      const contractBalance = await ethers.provider.getBalance(
+        contract.address,
+      );
+      expect(contractBalance.toString()).to.equal(
+        ethers.utils.parseEther("0.0025"),
+      );
+
+      const totalSupply = await contract.totalSupply();
+
+      expect(totalSupply.toString()).to.equal(
+        ethers.utils.parseEther("1.0058"),
+      );
+
+      const claimableBalance = await contract.balanceOf(contract.address);
+      expect(claimableBalance.toString()).to.equal("5800000000000000");
+    });
+    it("doesnt let not approved people rebalance");
+  });
+
+  describe("claiming", async () => {
+    it("claims correctly");
+    it("cant claim if not a benificiary");
+  });
+
+  describe("burning", async () => {
+    it("burns correctly");
+    it("cant burn tokens I dont own");
+    it("cant burn tokens not in reserves");
+    it("hard burns correclty");
   });
 });
